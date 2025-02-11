fbdf89dedc9839cc744af5bcd6ad2e1945d3daf5
drmingler <davidemmanuel75@gmail.com>
2025-02-11 13:42:30 +0000

    fix: Saving output files

--- llm_loader/document_loader.py
+++ llm_loader/document_loader.py
@@ -4,7 +4,7 @@ Document loader module for handling different types of inputs (files and URLs).
 import asyncio
 from pathlib import Path
 import tempfile
-from typing import AsyncIterator, List, Optional, Iterator, Union
+from typing import AsyncIterator, List, Optional, Iterator, Tuple, Union
 from base64 import b64encode
 import io
 from multiprocessing import cpu_count
@@ -16,6 +16,7 @@ from pdf2image import convert_from_path
 from pydantic import BaseModel
 from litellm import completion, validate_environment, supports_vision, check_valid_key, acompletion
 import requests
+import shutil
 
 
 def is_pdf(url: str, response: requests.Response) -> bool:
@@ -26,18 +27,15 @@ def is_pdf(url: str, response: requests.Response) -> bool:
     ]
 
 
-def save_output_file(documents: List[Document], file_path: Path, save_output: bool) -> None:
+def save_output_file(documents: List[Document], output_dir: Path) -> None:
+    print(f"Saving output to {output_dir}")
     """Save the chunks and input file to a folder."""
     import json
-    import shutil
 
-    if not save_output:
+    if not output_dir:
         return
 
-    input_file = Path(file_path)
-    output_dir = input_file.parent / input_file.stem
     output_dir.mkdir(exist_ok=True)
-
     chunks_data = [
         {
             "content": doc.page_content,
@@ -46,13 +44,10 @@ def save_output_file(documents: List[Document], file_path: Path, save_output: bo
         for doc in documents
     ]
 
-    chunks_file = output_dir / f"{input_file.stem}_chunks.json"
+    chunks_file = output_dir / f"{output_dir.stem}_chunks.json"
+    print(f"Saving chunks to {chunks_file}")
     with open(chunks_file, "w", encoding="utf-8") as f:
         json.dump(chunks_data, f, indent=2, ensure_ascii=False)
-
-    if input_file.exists():
-        output_file = output_dir / input_file.name
-        shutil.move(input_file, output_file)
 
 
 DEFAULT_CHUNK_PROMPT = """OCR the following page into Markdown. Tables should be formatted as HTML.
@@ -64,10 +59,10 @@ be embedded and used in a RAG pipeline.
 """
 
 DEFAULT_PAGE_CHUNK_PROMPT = """OCR the following page into Markdown. Tables should be formatted as HTML.
-Do not surround your output with triple backticks. The contents of the page should be returned as a single chunk. 
+Do not surround your output with triple backticks. The contents of the page should be returned as a single chunk.
 Also return the semantic theme of the page.
 
-Images in the document should be properly discribed in details such that an LLM can understand the image and answer 
+Images in the document should be properly discribed in details such that an LLM can understand the image and answer
 questions about the image without seeing the image.
 The description should be returned as a part of the page content.
 """
@@ -93,9 +88,9 @@ class ImageProcessor:
             size=(None, 1056),
             thread_count=cpu_count(),
             use_pdftocairo=True,
-            )
+        )
         return images
-    
+
     @staticmethod
     def image_to_base64(image: Image) -> str:
         """Convert an image to a base64 string."""
@@ -183,7 +178,7 @@ class LLMProcessing:
         file_path: Optional[Union[str, Path]] = None,
         chunk_strategy: str = 'page',
         custom_prompt: Optional[str] = None,
-        save_output: bool = False,
+        output_dir: Optional[Union[str, Path]] = None,
     ) -> List[Document]:
         """Process a document with LLM for OCR and chunking."""
 
@@ -194,7 +189,7 @@ class LLMProcessing:
 
         results = asyncio.run(process_pdf())
         documents = self.serialize_response(list(results), file_path)
-        save_output_file(documents, file_path, save_output)
+        save_output_file(documents, output_dir)
         return documents
 
     async def async_process_document_with_llm(
@@ -202,14 +197,14 @@ class LLMProcessing:
         file_path: Optional[Union[str, Path]] = None,
         chunk_strategy: str = 'page',
         custom_prompt: Optional[str] = None,
-        save_output: bool = False,
+        output_dir: Optional[Union[str, Path]] = None,
     ) -> List[Document]:
         """Process a document with LLM for OCR and chunking asynchronously."""
         images = ImageProcessor.pdf_to_images(file_path)
         prompt = self.get_chunk_prompt(chunk_strategy, custom_prompt)
         results = list(await asyncio.gather(*[self.async_process_with_llm(img, prompt) for img in images]))
         documents = self.serialize_response(list(results), file_path)
-        save_output_file(documents, file_path, save_output)
+        save_output_file(documents, output_dir)
         return documents
 
     async def async_process_with_llm(self, page_as_image: Image, prompt: str) -> dict:
@@ -276,7 +271,6 @@ class DocumentLoader(BaseLoader):
         """
         self.chunk_strategy = chunk_strategy
         self.custom_prompt = custom_prompt
-        self.save_output = save_output
         self.llm_processor = LLMProcessing(model=model, **kwargs)
 
         if file_path and url:
@@ -285,19 +279,27 @@ class DocumentLoader(BaseLoader):
         if not file_path and not url:
             raise ValueError("Either file_path or url must be provided.")
 
-        self.file_path = self._load_from_path(file_path) if file_path else self._load_from_url(url)
+        self.file_path, self.output_dir = (
+            self._load_from_path(file_path, save_output) if file_path else self._load_from_url(url, save_output)
+        )
 
     @staticmethod
-    def _load_from_path(file_path: Union[str, Path]) -> Path:
+    def _load_from_path(file_path: Union[str, Path], save_output: bool = False) -> Tuple[Path, Optional[Path]]:
         """Load documents from a file path."""
         file_path = Path(file_path)
         if not file_path.exists():
             raise FileNotFoundError(f"File not found: {file_path}")
 
-        return file_path
+        output_dir = file_path.parent / file_path.stem
+        if save_output:
+            output_dir.mkdir(exist_ok=True)
+            output_file = output_dir / file_path.name
+            shutil.copy2(file_path, output_file)
+
+        return file_path, output_dir
 
     @staticmethod
-    def _load_from_url(url: str) -> Path:
+    def _load_from_url(url: str, save_output: bool = False) -> Tuple[Path, Optional[Path]]:
         """Load documents from a URL."""
         response = requests.get(url)
         response.raise_for_status()
@@ -306,16 +308,25 @@ class DocumentLoader(BaseLoader):
         if is_link_to_pdf:
             with tempfile.NamedTemporaryFile(suffix='.pdf', delete=False) as temp_file:
                 temp_path = Path(temp_file.name)
+                temp_file.write(response.content)
 
-            temp_path.write_bytes(response.content)
-            return temp_path
+            output_dir = None
+            if save_output:
+                url_filename = url.split('/')[-1] or 'output'
+                url_filename = url_filename if ".pdf" in url_filename else url_filename + ".pdf"
+                output_dir = Path.cwd() / Path(url_filename).stem
+                output_dir.mkdir(exist_ok=True)
+                output_file = output_dir / url_filename
+                shutil.copy2(temp_path, output_file)
+
+            return temp_path, output_dir
         else:
             raise ValueError("The URL does not point to a PDF file.")
 
     async def aload(self) -> list[Document]:
         """Load Documents and split into chunks using LLM-based OCR processing. async version"""
         return await self.llm_processor.async_process_document_with_llm(
-            self.file_path, chunk_strategy="page", save_output=self.save_output
+            self.file_path, chunk_strategy="page", output_dir=self.output_dir
         )
 
     def load(self) -> List[Document]:
@@ -329,14 +340,14 @@ class DocumentLoader(BaseLoader):
             List of Document objects without chunked pages
         """
         documents = self.llm_processor.process_document_with_llm(
-            self.file_path, chunk_strategy="page", save_output=self.save_output
+            self.file_path, chunk_strategy="page", output_dir=self.output_dir
         )
         return documents
 
     def load_and_split(self, text_splitter: Optional = None) -> List[Document]:
         """Load Documents and split into chunks using LLM-based OCR processing."""
         documents = self.llm_processor.process_document_with_llm(
-            self.file_path, self.chunk_strategy, self.custom_prompt, save_output=self.save_output
+            self.file_path, self.chunk_strategy, self.custom_prompt, output_dir=self.output_dir
         )
         return documents
 
@@ -366,7 +377,7 @@ class DocumentLoader(BaseLoader):
                 documents.append(doc)
                 yield doc
 
-        save_output_file(documents, self.file_path, self.save_output)
+        save_output_file(documents, self.output_dir)
 
     async def alazy_load(self) -> AsyncIterator[Document]:
         """Load Documents lazily and asynchronously, processing and yielding one page at a time."""
@@ -383,4 +394,4 @@ class DocumentLoader(BaseLoader):
                 documents.append(doc)
                 yield doc
 
-        save_output_file(documents, self.file_path, self.save_output)
+        save_output_file(documents, self.output_dir)
