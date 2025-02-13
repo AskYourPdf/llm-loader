994a32c9f0822b6b42eeeeec00508b6b34ef6363
drmingler <davidemmanuel75@gmail.com>
2025-02-13 11:36:46 +0000

    feat: Change prompt

--- llm_loader/prompts.py
+++ llm_loader/prompts.py
@@ -1,8 +1,7 @@
-DEFAULT_CHUNK_PROMPT = """OCR the following page into Markdown. Tables should be formatted as HTML.
-Do not surround your output with triple backticks.
-
-Chunk the document into sections of roughly 250 - 1500 words. Our goal is
-to identify parts of the page with same semantic theme. These chunks will
+DEFAULT_CHUNK_PROMPT = """OCR the following page into Markdown format.
+- Do not exclude any content from the page.
+- Chunk the markdown into sections of roughly 250 - 1500 words with same semantic theme.
+- Our goal is to identify parts of the page with same semantic theme. These chunks will
 be embedded and used in a RAG pipeline.
 
 Images in the document should be properly described in details such that an LLM can understand the
@@ -10,11 +9,12 @@ image and answer questions about the image without seeing the image.
 The image description should be returned as a chunk too.
 """
 
-DEFAULT_PAGE_CHUNK_PROMPT = """OCR the following page into Markdown. Tables should be formatted as HTML.
-Do not surround your output with triple backticks. The contents of the page should be returned as a single chunk.
-Also return the semantic theme of the page.
+DEFAULT_PAGE_CHUNK_PROMPT = """OCR the following page into Markdown format. 
+- The contents of the page should be returned as a single chunk.
+- Do not exclude any content from the page.
+- Also return the semantic theme of the page.
 
-Images in the document should be properly discribed in details such that an LLM can understand the image and answer
+Images in the document should be properly described in details such that an LLM can understand the image and answer
 questions about the image without seeing the image.
 The description should be returned as a part of the page content.
 """
