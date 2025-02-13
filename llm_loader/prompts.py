78984eb31916d4d14422ee9f5e4cbbf3bb67aeba
drmingler <davidemmanuel75@gmail.com>
2025-02-13 14:28:39 +0000

    feat: Change prompts

--- llm_loader/prompts.py
+++ llm_loader/prompts.py
@@ -1,9 +1,10 @@
 DEFAULT_CHUNK_PROMPT = """OCR the following page into Markdown format. 
-- Tables must be formatted in Markdown.
+- All tables, charts, and other visual elements must be formatted in Markdown.
 - Do not exclude any content from the page.
-- Chunk the markdown into sections of roughly 250 - 1500 words with same semantic theme.
+- Chunk the page into sections with same semantic theme.
 - Our goal is to identify parts of the page with same semantic theme. These chunks will
 be embedded and used in a RAG pipeline.
+- All chunks must be in Markdown format.
 
 Images in the document should be properly described in details such that an LLM can understand the
 image and answer questions about the image without seeing the image.
@@ -11,6 +12,7 @@ The image description should be returned as a chunk too.
 """
 
 DEFAULT_PAGE_CHUNK_PROMPT = """OCR the following page into Markdown format. 
+- All tables must be formatted in Markdown.
 - The contents of the page should be returned as a single chunk.
 - Do not exclude any content from the page.
 - Also return the semantic theme of the page.
